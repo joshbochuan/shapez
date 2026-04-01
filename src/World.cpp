b6595b0f466b5bc8cc2f5762a23c5cd8e16dd24c
joshbochuan <joshbochuan@gmail.com>
2026-04-01 15:09:11 +0800

    multithreading

--- src/World.cpp
+++ src/World.cpp
@@ -3,25 +3,28 @@
 //
 
 #include "World.hpp"
+#include <execution>
 using namespace World;
 
 void World::OperateMachines() {
-    for (const auto& machine: LstMachines) {machine->Update();}
+    std::for_each(std::execution::par, LstMachines.begin(), LstMachines.end(),
+        [](const auto& machine) {
+            machine->Update();
+        });
 
     // moving items from ejectors to acceptors
-    std::shared_ptr<ItemAcceptor> acceptor;
-    std::shared_ptr<ItemEjector> ejector;
-    for (const auto& pair : MapEjectors) {
-        ejector = pair.second;
-        if (ejector == nullptr) {continue;}
-        if (ejector->item == nullptr) {continue;}
-        if (ejector->progress < 1) {continue;}
+    std::for_each(std::execution::par, MapEjectors.begin(), MapEjectors.end(),
+        [](const auto& pair) {
+        auto ejector = pair.second;
+        if (ejector == nullptr) {return;}
+        if (ejector->item == nullptr) {return;}
+        if (ejector->progress < 1) {return;}
 
-        acceptor = ejector->next;
-        if (acceptor == nullptr) {continue;}
-        if (acceptor->item != nullptr) {continue;}
-        if ((ejector->item->getType() == ItemType::COLOR) && (!acceptor->takesColor)) {continue;}
-        if ((ejector->item->getType() == ItemType::SHAPE) && (!acceptor->takesShape)) {continue;}
+        auto acceptor = ejector->next;
+        if (acceptor == nullptr) {return;}
+        if (acceptor->item != nullptr) {return;}
+        if ((ejector->item->getType() == ItemType::COLOR) && (!acceptor->takesColor)) {return;}
+        if ((ejector->item->getType() == ItemType::SHAPE) && (!acceptor->takesShape)) {return;}
 
         acceptor->item = ejector->item;
         acceptor->progress = ejector->progress-1;
@@ -29,5 +32,5 @@ void World::OperateMachines() {
         ejector->RemoveChild(acceptor->item);
         ejector->item = nullptr;
         ejector->progress = 0;
-    }
+    });
 }
